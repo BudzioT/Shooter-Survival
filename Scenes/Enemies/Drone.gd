df8950a7256d842140bde456e07a82f482f4a920
BudzioT <Budzio.zsm1@gmail.com>
2024-07-13 11:20:54 +0200

    Added billboard, moving drone and gate. Added collisions, player movement

---
+++ Scenes/Enemies/Drone.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+# Update the Drone every frame
+func _process(_delta):
+	# Update its position
+	_update()
+	
+# Update the drone
+func _update():
+	# Get its direction
+	var direction = Vector2.RIGHT
+	
+	# Calculate the velocity
+	velocity = direction * 400
+	
+	# Move it and slide when colliding
+	move_and_slide()
+	
--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,1 +1,32 @@
-extends Node2D
+extends CharacterBody2D
+
+# Update the player in every frame
+func _process(delta):
+	# Update player's position
+	_update(delta)
+	
+	# Shoot the laser when needed
+	_main_action()
+	
+# Update player's position
+func _update(_delta):
+	# Get the player's direction vector
+	var direction = Input.get_vector("Left", "Right", "Up", "Down")
+	
+	# Update his velocity
+	velocity = direction * 400
+	
+	# Move the player, delta is applied automatically
+	move_and_slide()
+	
+# Shoot a  laser projectile from the weapon
+func _main_action():
+	# If player presses shoot action
+	if Input.is_action_pressed("MainAction"):
+		print("Laser")
+
+# Throw a grenade
+func _secondary_action():
+	# If user presses secondary action
+	if Input.is_action_pressed("SecondaryAction"):
+		print("Grenade")
