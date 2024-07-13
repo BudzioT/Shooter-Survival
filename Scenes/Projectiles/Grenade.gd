a94db299bef387ae2f57ea05b5e7ad5aa0883ed3
BudzioT <Budzio.zsm1@gmail.com>
2024-07-13 22:06:34 +0200

    Added fan animation and grenade blinking, explosion

--- Scenes/Projectiles/Grenade.gd
+++ Scenes/Projectiles/Grenade.gd
@@ -3,3 +3,11 @@ extends RigidBody2D
 
 # Speed of the grenade
 @export var speed : int = 400
+
+# Explode the grenade
+func explode():
+	# Make explosion visible
+	$Explosion.visible = true
+	
+	# Play the explosion animation
+	$AnimationPlayer.play("Explosion")
